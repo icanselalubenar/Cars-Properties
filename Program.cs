883538d32145eb295bdaac9a8a90710d0bca774b
Ikhsan Abdul Rafi <107069762+icanselalubenar@users.noreply.github.com>
2023-03-21 20:31:14 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Mobil
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Mengisi properti
+            carClass Car1 = new carClass();
+            Car1.Color = "Merah";
+            Car1.Brand = "Ferarri";
+            Car1.Type = "SF90";
+            Car1.Horn = "Ngonggg~";
+            Car1.Doors = 2;
+            Car1.Since = 2023;
+            Car1.Speed = 320;
+
+            // memanggil metode
+            Car1.Gas(Car1.Speed);
+            Car1.Klakson(Car1.Horn);
+            Car1.TampilkanInfo();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ carClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace Mobil
+{
+    public class carClass
+    {
+        // Properties
+        public string Color { get; set; }
+        public string Brand { get; set; }
+        public string Type { get; set; }
+        public string Horn { get; set; }
+        public int Doors { get; set; }
+        public int Since { get; set; }
+        public int Speed { get; set; }
+
+        // Method 1
+        public void Gas(int kecepatan)
+        {
+            Console.WriteLine("Method 1:");
+            Console.WriteLine("Mobil {0} berjalan dengan kecepatan {1} Km/h.\n", Type, kecepatan);
+        }
+        // Method 2
+        public void Klakson(string suara)
+        {
+            Console.WriteLine("Method 2:");
+            Console.WriteLine("{0}\n", suara);
+        }
+        // Method 3
+        public void TampilkanInfo()
+        {
+            Console.WriteLine("Method 3:");
+            Console.WriteLine("Mobil saya berwarna {0}, merek {1} model {2} keluaran tahun {3}\ndengan jumlah pintu sebanyak {4}.", Color, Brand, Type, Since, Doors);
+        }
+    }
+}
+
